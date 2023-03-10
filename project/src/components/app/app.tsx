992f4a4d4253577664a622e034bddbf32aac69ec
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2023-03-10 19:27:24 +0300

    Маршрут по тайным тропам  - В модуле компонента `App` подключаем компоненты `Routes`, `Route`, `BrowserRouter` из пакета `react-router-dom`. - Добавил маршрутизацию в приложение.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,47 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import WelcomePage from '../../pages/welcome-page/welcome-page';
+import WelcomeEmptyPage from '../../pages/welcome-empty-page/welcome-empty-page';
+import LoginPage from '../../pages/login-page/login-page';
+import OfferUserLoggedPage from '../../pages/offer-user-logged-page/offer-user-logged-page';
+import OfferUserNotLoggedPage from '../../pages/offer-user-not-logged-page/offer-usser-not-logged-page';
+import ErrorPage from '../../pages/error-page/error-page';
 
 type AppWelcomePageProps = {
   offerCount: number;
 }
 
 function App({offerCount}: AppWelcomePageProps): JSX.Element {
-  return < WelcomePage offerCount = {offerCount}/>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element = {<WelcomePage offerCount={offerCount}/>}
+        />
+        <Route
+          path={AppRoute.RootEmpty}
+          element = {<WelcomeEmptyPage/>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element = {<LoginPage/>}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element = {<OfferUserLoggedPage/>}
+        />
+        <Route
+          path={AppRoute.OfferNot}
+          element = {<OfferUserNotLoggedPage/>}
+        />
+        <Route
+          path="*"
+          element = {<ErrorPage/>}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ project/src/const.ts
@@ -0,0 +1,7 @@
+export enum AppRoute {
+  Root = '/',
+  RootEmpty = '/empty',
+  Login = '/login',
+  Offer = '/offer',
+  OfferNot = '/offernot',
+}
